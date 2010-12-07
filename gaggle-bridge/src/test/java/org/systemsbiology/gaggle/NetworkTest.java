b3469e65fe2d28215f9c0fae1b8a077f377781ef
Wei-ju Wu <wwu@systemsbiology.org>
2010-12-07 09:43:16 -0800

    added more test cases for network

--- gaggle-bridge/src/test/java/org/systemsbiology/gaggle/NetworkTest.java
+++ gaggle-bridge/src/test/java/org/systemsbiology/gaggle/NetworkTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import org.systemsbiology.gaggle.core.datatypes.Network;
 import org.systemsbiology.gaggle.core.datatypes.Interaction;
+import org.systemsbiology.gaggle.core.datatypes.Tuple;
 
 public class NetworkTest {
     @Test public void testCreateEmpty() {
@@ -23,5 +24,29 @@ public class NetworkTest {
         assertEquals(0, network.getEdgeAttributeNames().length);
 
         assertEquals("", network.toString());
+    }
+    @Test public void testSimpleSetters() {
+        Network network = new Network();
+        Tuple metadata = new Tuple();
+        network.setName("name");
+        network.setSpecies("species");
+        network.setMetadata(metadata);
+
+        assertEquals("name", network.getName());
+        assertEquals("species", network.getSpecies());
+        assertEquals(metadata, network.getMetadata());
+    }
+
+    @Test public void testAddNode() {
+        Network network = new Network();
+        network.add("node");
+        assertEquals(1, network.nodeCount());
+        assertEquals(1, network.getNodes().length);
+        assertEquals("node", network.getNodes()[0]);
+        assertEquals(0, network.edgeCount());
+        assertEquals(1, network.getOrphanNodeCount());
+        assertEquals(1, network.getOrphanNodes().length);
+        assertEquals("node", network.getOrphanNodes()[0]);
+        assertEquals(0, network.getConnectedNodes().size());
     }
 }
