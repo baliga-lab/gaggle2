80c06d7f2a0989c0aafa65ce56e30cd289c38a49
Wei-ju Wu <Weiju.Wu@gmail.com>
2011-05-03 16:27:00 -0700

    added documentation for HTTP related classes

--- java/gaggle-boss/src/main/java/org/systemsbiology/gaggle/boss/BossHttpServer.java
+++ java/gaggle-boss/src/main/java/org/systemsbiology/gaggle/boss/BossHttpServer.java
@@ -13,33 +13,54 @@ import org.mortbay.jetty.servlet.*;
 
 import org.systemsbiology.gaggle.core.*;
 
+/**
+ * Gaggle boss embeds Jetty Server in order to support Geese that connect
+ * through HTTP. It requires Jetty &gt;= 7.0 to use the Java Servlet 3.0
+ * compatible API for continuation-based Servlets.
+ * 
+ * This represents a platformneutral approach to connect to the Gaggle Boss.
+ * In order to support AJAX applications, BossHttpServer employs the HTTP long
+ * polling.
+ */
 class BossHttpServer extends HttpServlet {
 
+    private static final String HTTP_CONTEXT = "/";
+    private static final String SERVLET_PATTERN = "/*";
     private Server server;
     private BossImpl bossImpl;
 
+    /**
+     * Constructor.
+     * @param bossImpl the main boss implementation
+     * @param port the number of the port to run this service on
+     */
     public BossHttpServer(BossImpl bossImpl, int port) {
         this.bossImpl = bossImpl;
         server = new Server(port);
-        Context root = new Context(server, "/", Context.SESSIONS);
+        Context root = new Context(server, HTTP_CONTEXT, Context.SESSIONS);
         ServletHandler handler = new ServletHandler();
-        root.addServlet(new ServletHolder(this), "/*");
+        root.addServlet(new ServletHolder(this), SERVLET_PATTERN);
     }
 
+    /**
+     * Starts the HTTP service.
+     * @throws Exception if server could not be started successfully
+     */
     public void startListen() throws Exception {
         server.start();
     }
 
+    /** {@inheritDoc} */
     @Override public void doGet(final HttpServletRequest request,
                                 final HttpServletResponse response)
         throws IOException {
         if (request.isInitial()) {
             String command = request.getParameter("command");
+            String gooseName = request.getParameter("name");
             if ("register".equals(command)) {
-                String gooseName = registerHttpGoose(request.getParameter("name"));
-                setJSONResponse(response, jsonGooseName(gooseName));
+                String finalGooseName = registerHttpGoose(gooseName);
+                setJSONResponse(response, jsonGooseName(finalGooseName));
             } else if ("waitBroadcast".equals(command)) {
-                String gooseName = request.getParameter("name");
                 HttpGoose httpGoose = getHttpGooseFor(gooseName);
                 if (httpGoose != null) {
                     request.suspend();
@@ -47,13 +68,15 @@ class BossHttpServer extends HttpServlet {
                 } else {
                     System.out.println("HTTP Goose unavailable");
                 }
-                return;
             } else if ("doBroadcast".equals(command)) {
-                String gooseName = request.getParameter("name");
                 String jsonData = request.getParameter("data");
                 bossImpl.broadcastJSON(gooseName, "boss", jsonData);
                 setJSONResponse(response, "{\"status\":\"ok\"}");
+            } else {
+                // TODO: report unhandled command
             }
+        } else {
+            // TODO: report unhandled
         }
     }
 
--- java/gaggle-boss/src/main/java/org/systemsbiology/gaggle/boss/HttpGoose.java
+++ java/gaggle-boss/src/main/java/org/systemsbiology/gaggle/boss/HttpGoose.java
@@ -9,14 +9,35 @@ import org.mortbay.jetty.*;
 import org.systemsbiology.gaggle.core.*;
 import org.systemsbiology.gaggle.core.datatypes.*;
 
+/**
+ * A representation of a Gaggle Goose connected through HTTP.
+ * This class only deals with the receiving part of a Goose, because the
+ * BossHttpServer class deals with the broadcasting part.
+ */
 public class HttpGoose implements JSONGoose {
 
+    /**
+     * A RequestHandler is the means to push a broadcast to an HTTP goose.
+     * We assume that a connection is used in a long polling fashion.
+     * When data is broadcasted as a JSON string, the request is resumed and
+     * the broadcast data is set into the request.
+     */
     public static class RequestHandler {
         private HttpServletRequest request;
 
+        /**
+         * Constructor.
+         * @param request the HTTP request
+         */
         public RequestHandler(HttpServletRequest request) {
             this.request = request;
         }
+
+        /**
+         * Sends the specified JSON string to the associated HTTP connection.
+         * @param source the source goose
+         * @param json the JSON data
+         */
         public void handleJSON(String source, String json) throws IOException {
             System.out.printf("HttpGoose.handleJSON() [REQ: %s], source: '%s', json: '%s'\n",
                               request, source, json);
@@ -34,24 +55,24 @@ public class HttpGoose implements JSONGoose {
     private String[] knownGeese = new String[0];
     private RequestHandler currentHandler;
 
+    /**
+     * Constructor.
+     * @param originalName original goose name
+     */
     public HttpGoose(String originalName) {
         this.originalName = originalName;
         this.gooseName = originalName;
     }
-    public void doExit() { }
-    public void doShow() { }
-    public void doHide() { }
-    public void doBroadcastList() { }
-    public void setName(String newName) {
-        this.gooseName = newName;
-    }
-    public String getName() {
-        return gooseName;
-    }
+
+    /** {@inheritDoc} */
+    public void setName(String newName) { this.gooseName = newName; }
+    /** {@inheritDoc} */
+    public String getName() { return gooseName; }
+    /** {@inheritDoc} */
     public void update(String[] gooseNames) {
         this.knownGeese = gooseNames;
     }
-
+    /** {@inheritDoc} */
     public void handleJSON(String source,
                            String json) throws RemoteException {
         if (currentHandler == null) {
@@ -65,6 +86,10 @@ public class HttpGoose implements JSONGoose {
         }
     }
 
+    /**
+     * Sets a handler to wait for an incoming broadcast request.
+     * @param handler the broadcast request handler
+     */
     public void waitForBroadcast(RequestHandler handler) {
         this.currentHandler = handler;
     }
